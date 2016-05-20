5021e51fe3cd49e1269282eb5f27686f60a2fcd9
mroden <marky.roden@gmail.com>
2016-05-20 14:57:21 -0500

    Initial Commit

---
+++ AppRead/Home/Home.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="../App.js" />
+// global app
+
+(function () {
+    'use strict';
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            Office.context.mailbox.item.body.getAsync(
+			  "text",
+			  { asyncContext:"This is passed to the callback" },
+			  function callback(result) {
+			    // Do something with the result
+				var str = result.value; 
+				var res = str.match(/Workflow\sId:\s[0-9|a-z|A-Z]{6}/g).toString();      				
+				var wf = res.replace(/Workflow Id:\s/g, "WorkflowId=");
+				
+				$("#content-main").html("The RegEx matched is---> "+res)
+				
+				//You can then change it up to add to an IFrame, or an angular application route
+				//var url = "https://xomino365.azurewebsites.net/yourapplication?&"+wf + "&"
+				//$("#content-main").append("<iframe style='width: 100%; overflow: hidden' src='"+url+"' frameborder=0/>")
+			  });
+        });
+    };
+
+    // Displays the "Subject" and "From" fields, based on the current mail item
+    function displayItemDetails() {
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        $('#subject').text(item.subject);
+
+        var from;
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            from = Office.cast.item.toMessageRead(item).from;
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            from = Office.cast.item.toAppointmentRead(item).organizer;
+        }
+
+        if (from) {
+            $('#from').text(from.displayName);
+            $('#from').click(function () {
+                app.showNotification(from.displayName, from.emailAddress);
+            });
+        }
+    }
+})();
---
+++ README.md
@@ -0,0 +1,17 @@
+# RegEx In Outlook Add In Example
+Sample code for demonstrating how to use a Regular Expression inside of an O365 plugin to detect and use Dynamically created text related to a workflow application
+
+# What it does
+What it says on the tin :)
+
+# Instructions
+This sample code is intended to be used inside of a https://www.napacloudapp.com applicaiton for a developer learning O365 AddIns
+
+
+For more information check out the blog post - ???
+
+# Release notes
+
+Version 0.1
+* Initial release
+
